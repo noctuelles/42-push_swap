0cc97d965682895c640038d387a55c8ce296bc9a
noctuelles <plouvel@student.42.fr>
2022-02-22 13:15:57 +0100

    also corrected the checker

--- srcs/checker/operations/swap.c
+++ srcs/checker/operations/swap.c
@@ -6,7 +6,7 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 13:47:47 by plouvel           #+#    #+#             */
-/*   Updated: 2022/02/11 19:55:08 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/02/22 13:11:56 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	sb(t_data *data)
 {
 	t_element	tmp;
 
-	if (data->a.top < 2)
+	if (data->b.top < 2)
 		return ;
-	tmp = data->a.content[data->a.top - 1];
-	data->a.content[data->a.top - 1] = data->a.content[data->a.top - 2];
-	data->a.content[data->a.top - 2] = tmp;
+	tmp = data->b.content[data->b.top - 1];
+	data->b.content[data->b.top - 1] = data->b.content[data->b.top - 2];
+	data->b.content[data->b.top - 2] = tmp;
 }
 
 void	ss(t_data *data)
